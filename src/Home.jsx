ef8ae6f96106e8dddc19129fda19564016f239df
Abhishek <akraj618@gmail.com>
2023-07-29 18:04:58 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -98,7 +98,7 @@ const Home = () => {
   return (
     <div className="flex items-center justify-center w-screen min-h-screen pt-16 pb-12 font-medium">
       <div className="flex flex-grow items-center justify-center bg-gray-900 h-full">
-        <div className="max-w-full p-8 bg-gray-800 rounded-lg shadow-lg sm:w-xs text-gray-200">
+        <div className="max-w-full p-8 bg-gray-800 rounded-lg shadow-lg w-xs sm:w-3/4 md:w-1/2 text-gray-200">
           <div className="flex items-center mb-6">
             <svg className="h-8 w-8 text-indigo-500 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
